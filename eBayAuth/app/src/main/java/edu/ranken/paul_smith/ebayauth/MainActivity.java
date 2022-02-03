767c6c23981dbf8be573ed6eb2876b888dc4096d
Paul Smith <prsmith@ranken.edu>
2022-02-03 17:35:51 -0600

    ebay auth project created

---
+++ eBayAuth/app/src/main/java/edu/ranken/paul_smith/ebayauth/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.ranken.paul_smith.ebayauth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
